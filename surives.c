ece9ecd3f7b56a102013f84be54177a0246a9cc0
LiSepp <b15011515@njupt.edu.cn>
2016-07-17 11:56:48 +0800

    1·

---
+++ surives.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+    int f2,f3,f4,i=2;
+    f2=f3=f4=0;
+    f2=1;
+    f3=1;
+    while(1)
+    {f4=f2+f3;++i;if(i==N){ printf("%d\n",f4);break;}
+    f2=f4+f3;++i;if(i==N){ printf("%d\n",f2);break;}
+    f3=f2+f4;++i;if(i==N){ printf("%d\n",f3);break;}
+    } 
+    return 0;
+}
+int main()
+{   
+    int N;
+    scanf("%d\n",&N);
+    fl(N);
+    return 0;
+}
